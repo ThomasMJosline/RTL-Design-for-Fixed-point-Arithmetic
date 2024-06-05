16dad15647e5fea16272c53fc69a9e692d666a39
ThomasMJosline <84652232+ThomasMJosline@users.noreply.github.com>
2024-06-05 16:09:21 +0530

    Update main module

--- Fixed Point addition/fixed_point_adder.v
+++ Fixed Point addition/fixed_point_adder.v
@@ -1,1 +1,45 @@
+module fp_adder #(
+    parameter W_in =16 ,  //!Word length of inputs 
+    parameter W_in_F = 14, //! Length of fractional part of output
+    parameter W_out =16 , //!Word length of output
+    parameter W_out_F = 14 //! Length of fractional part of output
+) (
+    input signed [W_in-1:0] a, 
+    input signed [W_in-1:0] b,
+    output reg signed [W_out-1:0] sum,
+    output reg overflow, //!Shows whether overflow have occured or not
+    output reg underflow //!Shows whether underflow have occured or not
+);
 
+always @(*) begin
+    sum = a + b;
+    if (a[W_in-1] != b[W_in-1])
+    begin 
+        overflow = 0;
+        underflow = 0;
+    end 
+    else begin
+        if (a[W_in-1] == 0 ) begin
+            if (sum[W_in-1]==1) begin
+                overflow = 1'b1;
+                underflow = 0;   
+            end
+            else begin
+                overflow = 0;
+                underflow = 0;
+            end
+        end
+        else begin
+            if (sum[W_in-1]==1) begin
+                overflow = 0;
+                underflow = 0;   
+            end
+            else begin
+                overflow = 0;
+                underflow = 1'b1;
+            end
+        end
+    end
+end
+
+endmodule
